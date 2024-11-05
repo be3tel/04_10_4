6d0737a7a1eb972c7b1b70d1d5bebf0a3d501e2b
be3tel <158083247+be3tel@users.noreply.github.com>
2024-11-05 08:25:19 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите количество дней");
+        int rasxod = scanner.nextInt();
+
+        int size = rasxod;
+        int array [] = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            Scanner scanner2 = new Scanner(System.in);
+            System.out.println("Введите расходы");
+            int value = scanner2.nextInt();
+            array[i] = value;
+        }
+
+        System.out.print ("Вывод элементов:");
+        for (int i = 0; i < size; i++) {
+            System.out.print (" " + array[i]);
+        }
+    }
+}
